a7bcc1d52e312e40513f5f2a58c4a5f0a848307a
Marcos de Araujo Batista <83717885+MarcosABatista@users.noreply.github.com>
2022-09-13 01:28:46 -0300

    Add files via upload

---
+++ Banco/Banco/Conta.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banco
+{
+    public class Conta
+    {
+        public int Numero { get; private set; }
+        public string Nome { get; set; }
+        public double Saldo { get; private set; }       
+
+        public Conta(int numero, string nome)
+        {
+            Numero = numero;
+            Nome = nome;
+        }
+
+        public Conta(int numero, string nome, double saldo) : this(numero, nome)
+        {
+        Saldo = saldo;
+        }
+
+        public void Deposito (double quantia)
+        {
+            Saldo += quantia;
+        }
+        public void Saque (double quantia)
+        {
+            Saldo -= quantia + 5;
+        }
+
+
+        public override string ToString()
+        {
+            return "Conta: "
+                + Numero
+                + " Nome: "
+                + Nome
+                + " saldo: $ "
+                + Saldo.ToString("f2");
+        }
+    }
+}
---
+++ Banco/Banco/Program.cs
@@ -0,0 +1,48 @@
+﻿/*program.cs */
+using Banco;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        Conta conta1;
+        Console.WriteLine("Numero da conta: ");
+        int numero = int.Parse(Console.ReadLine());
+        Console.WriteLine("Nome do titular da conta:");
+        string nome = Console.ReadLine();
+        Console.WriteLine("Havera deposito inicial(S/N)?");       
+        string resp = (Console.ReadLine());       
+        
+        if (resp == "s" || resp == "S")
+        {
+            Console.WriteLine("entre com o deposito inicial");
+            double deposito = double.Parse(Console.ReadLine());
+            conta1 = new Conta(numero, nome, deposito);
+        }
+        else
+        {
+          conta1 = new Conta (numero, nome);
+        }
+
+        Console.WriteLine();
+        Console.WriteLine("dados da conta");
+        Console.WriteLine(conta1);
+
+        Console.WriteLine();
+
+        Console.WriteLine("entre um valor para deposito: ");
+        double quantia = double.Parse(Console.ReadLine());
+        conta1.Deposito(quantia);
+        Console.WriteLine("Dados da conta atualizados: ");
+        Console.WriteLine(conta1);
+
+        Console.WriteLine();
+
+        Console.WriteLine("entre um valor para saque: ");
+        quantia = double.Parse(Console.ReadLine());
+        conta1.Saque(quantia);
+        Console.WriteLine("Dados da conta atualizados: ");
+        Console.WriteLine(conta1);
+    }
+}
